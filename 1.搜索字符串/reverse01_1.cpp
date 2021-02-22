a9376dd56a3b64875c2dd0cd815382ad5377faf8
Jack Tsai <64248906+JackCobra11@users.noreply.github.com>
2021-02-22 09:45:40 +0800

    Add files via upload

---
+++ 1.搜索字符串/reverse01_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int password;
+lable1:
+	cout << "请输入：";
+	cin >> password;
+	if (password == 114514)
+	{
+		cout << "输入正确"<<endl;
+	}
+	else
+	{
+		cout << "输入错误"<<endl;
+		goto lable1;
+	}
+	return 0;
+}
+
